98283ff63d5cc10532a404848209f17f8a1ae2ac
Hachibkhan <hachib.khan19@gmail.com>
2019-05-15 20:38:01 +0600

    Multiplication_table

---
+++ al.py
@@ -0,0 +1,18 @@
+def multiplication_table(n = 1):
+    
+    i = 1
+
+    r = range(10)
+
+    for i in r:
+
+        i = i + 1
+
+        print(n, "*", i, "=", n * i)
+
+
+
+multiplication_table(5)
+
+
+
